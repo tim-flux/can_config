b39e5a49f1b429a726c025f45b42cc651306db93
tim <tim@fluxrobotics.ai>
2022-03-11 15:32:57 +1030

    Added sudo

--- set_can.sh
+++ set_can.sh
@@ -2,6 +2,6 @@
 
 sudo cp ./start_can.service /etc/systemd/system/
 sudo cp ./enable_CAN.sh /enable_CAN.sh
-chmod +x /enable_CAN.sh
+sudo chmod +x /enable_CAN.sh
 sudo systemctl start start_can.service
 
