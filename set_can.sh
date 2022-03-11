5c2658fb36b7c13381d7f1c835a687bfa6d08561
tim <tim@fluxrobotics.ai>
2022-03-11 15:36:23 +1030

    Making permissions right

--- set_can.sh
+++ set_can.sh
@@ -3,5 +3,6 @@
 sudo cp ./start_can.service /etc/systemd/system/
 sudo cp ./enable_CAN.sh /enable_CAN.sh
 sudo chmod +x /enable_CAN.sh
+sudo systemctl daemon-reload
 sudo systemctl start start_can.service
 
